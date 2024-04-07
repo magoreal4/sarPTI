14de8d5149de0ab8d9aeff038299f9a953179133
magoreal4 <magoreal4@gmail.com>
2024-04-06 23:39:54 -0300

    fist commit

--- config/settings/base.py
+++ config/settings/base.py
@@ -121,15 +121,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # APPEND_SLASH = True
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
-}
-
-from datetime import timedelta
-
-SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-}
