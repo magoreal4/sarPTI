ee5f8637fe5995bad0331ec8c276bcc7635bb815
magoreal4 <magoreal4@gmail.com>
2024-05-01 16:46:57 -0400

    backup

--- config/urls.py
+++ config/urls.py
@@ -4,14 +4,19 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-# from main.admin import admin_site 
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
+    path('accounts/password_reset/', auth_views.PasswordResetView.as_view(), name='admin_password_reset'),
     path("admin/", admin.site.urls),
     # path('admin/', admin_site.urls),
     path('', include('main.urls')),
     path('', include('registros.urls')),
     path('', include('registrosgab.urls')),
+    path("accounts/", include("django.contrib.auth.urls")),
+    
+    path('login/', auth_views.LoginView.as_view(), name='login'),
     ]
 
 if settings.DEBUG:
--- main/urls.py
+++ main/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path, include
 from . import views
-from django.contrib.auth import views as auth_views
+
 
 app_name = 'main'  # Define el espacio de nombres aquí
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
-    path("accounts/", include("django.contrib.auth.urls")),
+
     
-    path('accounts/password_reset/', auth_views.PasswordResetView.as_view(), name='admin_password_reset'),
     
     # # Asegúrate de incluir también las URLs para el proceso completo de restablecimiento
     # path('admin/password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
