e939d5df9a7a357df5048524a467cf3cec9de8a6
magoreal4 <magoreal4@gmail.com>
2024-05-03 16:02:22 -0400

    backup

--- backup.sh
+++ backup.sh
@@ -2,19 +2,19 @@
 
 echo "Creando backup de la base de datos..."
 
-sudo docker-compose exec sar-django python manage.py dumpdata main.empresa --indent 2 > 01_empresa.json
+sudo docker-compose exec sar-django python manage.py dumpdata main.empresa --indent 2 > 01_empresa.json --settings=config.settings.prod
 echo "Base de empresa creada..."
 
-sudo docker-compose exec sar-django python manage.py dumpdata auth.user auth.group main.userprofile --indent 2 > 02_usuarios.json
+sudo docker-compose exec sar-django python manage.py dumpdata auth.user auth.group main.userprofile --indent 2 > 02_usuarios.json --settings=config.settings.prod
 echo "Base de usuarios creada..."
 
-sudo docker-compose exec sar-django python manage.py dumpdata main.sitio --indent 2 > 03_sitios.json
+sudo docker-compose exec sar-django python manage.py dumpdata main.sitio --indent 2 > 03_sitios.json --settings=config.settings.prod
 echo "Base de datos sitios creada..."
 
-sudo docker-compose exec sar-django python manage.py dumpdata registros --indent 2 > 04_registroscampo.json
+sudo docker-compose exec sar-django python manage.py dumpdata registros --indent 2 > 04_registroscampo.json --settings=config.settings.prod
 echo "Base de registros de campo creada..."
 
-sudo docker-compose exec sar-django python manage.py dumpdata regostrosgab --indent 2 > 05_registrosgab.json
+sudo docker-compose exec sar-django python manage.py dumpdata regostrosgab --indent 2 > 05_registrosgab.json --settings=config.settings.prod
 echo "Base de registros de gabinete creada..."
 
 echo "Archivos creados"
