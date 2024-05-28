2cd3661ece10a61968710ff350388fd3f8b7aa1f
magoreal4 <magoreal4@gmail.com>
2024-05-28 19:47:10 -0400

    estilos recuperados

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -18,9 +18,6 @@ MIDDLEWARE += [
     # "whitenoise.middleware.WhiteNoiseMiddleware",
     ]  
 
-
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
