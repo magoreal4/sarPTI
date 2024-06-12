8a01d70395439c20b20c0b9e15cb477ed7d366c2
magoreal4 <magoreal4@gmail.com>
2024-06-12 11:22:16 -0400

    backups

--- backup.sh
+++ backup.sh
@@ -5,16 +5,22 @@ echo "Creando backup de la base de datos..."
 sudo docker-compose exec sar-django python manage.py dumpdata main.empresa --indent 2 > 01_empresa.json --settings=config.settings.prod
 echo "Base de empresa creada..."
 
-sudo docker-compose exec sar-django python manage.py dumpdata auth.user auth.group main.userprofile --indent 2 > 02_usuarios.json --settings=config.settings.prod
+sudo docker-compose exec sar-django python manage.py dumpdata auth.Group --indent 2 > 02_groups.json --settings=config.settings.prod
+echo "Base de grupos creada..."
+
+sudo docker-compose exec sar-django python manage.py dumpdata auth.Permission --indent 2 > 03_permisos.json --settings=config.settings.prod
+echo "Base de permisos creada..."
+
+sudo docker-compose exec sar-django python manage.py dumpdata auth.user main.userprofile --indent 2 > 04_usuarios.json --settings=config.settings.prod
 echo "Base de usuarios creada..."
 
-sudo docker-compose exec sar-django python manage.py dumpdata main.sitio --indent 2 > 03_sitios.json --settings=config.settings.prod
+sudo docker-compose exec sar-django python manage.py dumpdata main.sitio --indent 2 > 05_sitios.json --settings=config.settings.prod
 echo "Base de datos sitios creada..."
 
-sudo docker-compose exec sar-django python manage.py dumpdata registros --indent 2 > 04_registroscampo.json --settings=config.settings.prod
+sudo docker-compose exec sar-django python manage.py dumpdata registros --indent 2 > 06_registroscampo.json --settings=config.settings.prod
 echo "Base de registros de campo creada..."
 
-sudo docker-compose exec sar-django python manage.py dumpdata registrosgab --indent 2 > 05_registrosgab.json --settings=config.settings.prod
+sudo docker-compose exec sar-django python manage.py dumpdata registrosgab --indent 2 > 07_registrosgab.json --settings=config.settings.prod
 echo "Base de registros de gabinete creada..."
 
 echo "Archivos creados"
--- restore.sh
+++ restore.sh
@@ -5,14 +5,20 @@ echo "Recuperando backup de la base de datos..."
 python manage.py loaddata 01_empresa.json
 echo "Base de empresa recuperada..."
 
-python manage.py loaddata 02_usuarios.json
+python manage.py loaddata 02_groups.json
 echo "Base de usuarios recuperada..."
 
-python manage.py loaddata 03_sitios.json
+python manage.py loaddata 03_permisos.json
 echo "Base de sitios recuperada..."
 
-python manage.py loaddata 04_registroscampo.json
+python manage.py loaddata 04_usuarios.json
 echo "Base de registros de campo recuperada..."
 
-python manage.py loaddata 05_registrosgab.json
+python manage.py loaddata 05_sitios.json
 echo "Base de registros de gabinete recuperada..."
+
+python manage.py loaddata 06_registroscampo.json
+echo "Base de registros de Politicas recuperada..."
+
+python manage.py loaddata 07_registrosgab.json
+echo "Base de registros de Terminos recuperada..."
