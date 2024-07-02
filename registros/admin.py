d5996d44bea0edb182d80df6bbf7482a3cd117d8
magoreal4 <magoreal4@gmail.com>
2024-07-01 23:54:53 -0400

    propietariotelefono

--- registros/admin.py
+++ registros/admin.py
@@ -222,17 +222,20 @@ class RegistroPropietarioAdmin(admin.ModelAdmin):
                     'propietario_nombre_apellido',
                     'propietario_telf',
                     )
-    # fields = ("__all__",)
-        # 'sitio',
-        # 'candidato',
-        # 'propietario_nombre_apellido',
-        # 'propietario_born',
-        # 'propietario_estado_civil',
-        # 'propietario_ci',
-        # 'propietario_telf',
-        # 'propietario_direccion',
-        # )
-
+    list_editable = ('propietario_telf',)
+    readonly_fields = ('propietario_tipo_persona',
+                       'propietario_nombre_apellido',
+                       'propietario_born',
+                       'propietario_ci',
+                    #    'propietario_telf',
+                       'propietario_direccion',
+                       'propietario_estado_civil',
+                       'propietario_email',
+                       'contacto_nombre',
+                       'contacto_tel',
+                       'contacto_email',
+                       'contacto_relacion'
+                    )
 admin.site.register(RegistroPropietario, RegistroPropietarioAdmin)
 
 
